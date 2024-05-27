6c686b02dec0d319c3b7e18ad0c69a23eb507585
magix-bugs <davienjenga37@gmail.com>
2024-05-27 17:55:17 +0300

    Rolling back to previous commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const generateReportHandler = require('/functions/generate-report');
+const generateReportHandler = require('./functions/generate-report');
 
 app.use('./netlify/functions/generate-report', generateReportHandler);
 

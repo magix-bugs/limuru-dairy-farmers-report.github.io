2df14e87419c51faa86a4ba275cf94a910320cb3
magix-bugs <davienjenga37@gmail.com>
2024-05-27 14:15:58 +0300

    Added webpack configuration and updated the generate report function

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './server.js', // Entry point for your server-side code
+    function: './functions/generate-report.js', // Entry point for your additional JavaScript file
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js', // Use [name] placeholder to output files with different names
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // Path to your HTML template file
+      filename: 'index.html', // Name of the output HTML file
+    }),
+  ],
+  resolve: {
+    fallback: {
+      "url": require.resolve("url/"),
+      "buffer": require.resolve("buffer/"),
+      "path": require.resolve("path-browserify"),
+      "util": require.resolve("util/"),
+      "stream": require.resolve("stream-browserify"),
+      "crypto": require.resolve("crypto-browserify/"),
+      "os": require.resolve("os-browserify/browser"),
+      "http": require.resolve("stream-http"),
+      "querystring": require.resolve("querystring-es3"),
+      "vm": require.resolve("vm-browserify"),
+      "zlib": require.resolve("browserify-zlib")
+    }
+  },
+  target: 'node' // Set webpack target to 'node'
+};

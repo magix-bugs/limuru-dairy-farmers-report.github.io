6ceb0e25c3d3f0cc554486cf2605603507410e33
magix-bugs <davienjenga37@gmail.com>
2024-05-27 17:17:03 +0300

    updated my server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const generateReportHandler = require('./functions/generate-report');
+const generateReportHandler = require('./netlify/functions/generate-report');
 
-app.use('/.netlify/functions/generate-report', generateReportHandler);
+app.use('./netlify/functions/generate-report', generateReportHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
